1bbfd7b426280ef80ced37ffe725c5e53f88a075
svmahur2004-stack <svmahur2004@gmail.com>
2026-01-06 11:46:28 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import check_voting_eligibility
 
 def test_eligible_voter():
-    assert check_voting_eligibility("anu", 20) == "Anu is eligible to vote"
+    assert check_voting_eligibility("anu", "Voting", 20) == "anu is eligible to vote"
 
 def test_not_eligible_voter():
-    assert check_voting_eligibility("Ravi", 16) == "Ravi is not eligible to vote"
+    assert check_voting_eligibility("Ravi", "Voting", 16) == "Ravi is not eligible to vote"
